9c0299b329272394128973008bc7c8ebb37be79f
Rohan Sood <rohansood1987@gmail.com>
2014-03-11 14:53:36 -0400

    Player V1 tests

--- jasmine_server_tests/spec/Player_v1_specs.js
+++ jasmine_server_tests/spec/Player_v1_specs.js
@@ -1,4 +1,18 @@
-var playerApiUrl = "http://1.smg-server.appspot.com/players"
+var playerApiUrl = 'http://1.smg-server.appspot.com/players'
+
+var error_emailExists = "EMAIL_EXISTS"
+var success_updatedPlayer = "UPDATED_PLAYER"
+var error_wrongAccessSig = "WRONG ACCESS SIGNATURE"
+var error_wrongPlayerID = "WRONG PLAYER ID"
+
+var playerId_correct = "42";
+var playerId_wrong = "50";
+
+var addNewPlayerRequest = '{email":"newPlayer@gmail.com","password":"1234","firstName":"New",lastName:"Player"}'
+var addExistingPlayerRequest = '{"email":"existingPlayer@gmail.com","password":"1234","firstName":"Existing",lastName:"Player"}'
+
+var updatePlayerInfoRequest = '{"accessSignature":"correct","email":"newPlayer123@gmail.com","password":"12345","firstName":"New1",lastName:"Player1"}'
+var updatePlayerInfoWrongSigRequest = '{"accessSignature":"wrong","email":"newPlayer123@gmail.com","password":"12345","firstName":"New1",lastName:"Player1"}'
 
 
 describe("Player", function() {
@@ -14,8 +28,77 @@ describe("Player", function() {
 
 	it("Add a new player", function() {
 		var request = new XMLHttpRequest();
-		
-
+		request.onreadystatechange = function() {
+			expect(request.status).toEqual(200);
+			var response = request.response;
+			
+			// Check return params
+			expect(request.response.has("playerId")).toBe(true);
+			
+		}
+		request.open("PUT", playerApiUrl, true);
+		request.responseType = "json";
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(addNewPlayerRequest);
 	})
 
+	it("Add an existing player", function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			expect(request.status).toEqual(200);
+			var response = request.response;
+			
+			// Check return params
+			expect(request.response.get("error")).toEqual(emailExistsError);
+		}
+		request.open("PUT", playerApiUrl, true);
+		request.responseType = "json";
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(addNewPlayerRequest);
+	})
+
+	it("Update player information", function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			expect(request.status).toEqual(200);
+			var response = request.response;
+			
+			// Check return params
+			expect(request.response.get("success")).toEqual(success_updatedPlayer);
+		}
+		request.open("PUT", playerApiUrl + "/" + playerId_correct, true);
+		request.responseType = "json";
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(updatePlayerInfoRequest);
+	})
+
+	it("Update player information with wrong Access Signature", function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			expect(request.status).toEqual(200);
+			var response = request.response;
+			
+			// Check return params
+			expect(request.response.get("error")).toEqual(error_wrongAccessSig);
+		}
+		request.open("PUT", playerApiUrl + "/" + playerId_correct, true);
+		request.responseType = "json";
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(updatePlayerInfoWrongSigRequest);
+	})
+
+	it("Update player information with wrong Player ID", function() {
+		var request = new XMLHttpRequest();
+		request.onreadystatechange = function() {
+			expect(request.status).toEqual(200);
+			var response = request.response;
+			
+			// Check return params
+			expect(request.response.get("error")).toEqual(error_wrongPlayerID);
+		}
+		request.open("PUT", playerApiUrl + "/" + playerId_wrong, true);
+		request.responseType = "json";
+		request.setRequestHeader("Content-type", "application/json");
+		request.send(updatePlayerInfoRequest);
+	})
 })
