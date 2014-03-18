43ebd29df0961648bdf812bef5f66e5cb262f887
rohansood1987 <rohansood1987@gmail.com>
2014-03-18 19:53:41 -0400

    Added related GameApi classes.

---
+++ SMG GWT Emulator/src/org/smg/gwt/emulator/data/ContainerConnector.java
@@ -0,0 +1,29 @@
+package org.smg.gwt.emulator.data;
+
+import java.util.List;
+
+import org.smg.gwt.emulator.data.GameApi.Container;
+import org.smg.gwt.emulator.data.GameApi.Operation;
+import org.smg.gwt.emulator.data.GameApi.VerifyMoveDone;
+
+public class ContainerConnector implements Container {
+
+  @Override
+  public void sendGameReady() {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void sendVerifyMoveDone(VerifyMoveDone verifyMoveDone) {
+    // TODO Auto-generated method stub
+
+  }
+
+  @Override
+  public void sendMakeMove(List<Operation> operations) {
+    // TODO Auto-generated method stub
+
+  }
+
+}
---
+++ SMG GWT Emulator/src/org/smg/gwt/emulator/data/GameApiJsonHelper.java
@@ -0,0 +1,17 @@
+package org.smg.gwt.emulator.data;
+
+import org.smg.gwt.emulator.data.GameApi.Message;
+
+public class GameApiJsonHelper {
+  
+  static String getJsonString(Message messageObject) {
+    //TODO
+    return null;
+  }
+  
+  static Message getJsonString(String jsonString) {
+    //TODO
+    return null;
+  }
+
+}
