7a54987e9ccca0678127db633ad5d84530f40700
Vishal Shah <vishal.shah@nyu.edu>
2014-04-22 01:53:47 -0400

    Adding mgwt settings

--- SMG GWT Emulator/src/org/smg/gwt/emulator/client/GwtEmulatorEntryPoint.java
+++ SMG GWT Emulator/src/org/smg/gwt/emulator/client/GwtEmulatorEntryPoint.java
@@ -3,6 +3,10 @@ package org.smg.gwt.emulator.client;
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.RootPanel;
+import com.googlecode.mgwt.mvp.client.Animation;
+import com.googlecode.mgwt.ui.client.MGWT;
+import com.googlecode.mgwt.ui.client.MGWTSettings;
+import com.googlecode.mgwt.ui.client.animation.AnimationHelper;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -15,10 +19,16 @@ public class GwtEmulatorEntryPoint implements EntryPoint {
    */
   public void onModuleLoad() {
     
+    // set viewport and other settings for mobile
+    MGWT.applySettings(MGWTSettings.getAppSetting());
+
+    // build animation helper and attach it
+    AnimationHelper animationHelper = new AnimationHelper();
+    RootPanel.get().add(animationHelper);
+
     GwtEmulatorGraphics graphicsLayout = new GwtEmulatorGraphics();
     
-    FlowPanel flowPanel = new FlowPanel();
-    flowPanel.add(graphicsLayout);
-    RootPanel.get("mainDiv").add(flowPanel);
+    // animate
+    animationHelper.goTo(graphicsLayout, Animation.SLIDE);
   }
 }
